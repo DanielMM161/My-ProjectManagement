db8f795b7b163845e455310efe1c123091a1be12
Daniel Moreno Marquez <daniel-moreno-marquez@hotmail.es>
2023-02-12 17:19:37 +0200

    Installing stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
